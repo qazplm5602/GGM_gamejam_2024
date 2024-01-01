5a35e9ddc481bb9968f3accd5311162203349bdd
domi <domi@domi.kr>
2024-01-02 03:06:20 +0900

    intro 빛빛나

--- Assets/Intro/IntroBloomEffect.cs
+++ Assets/Intro/IntroBloomEffect.cs
@@ -3,29 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class IntroBloomEffect : MonoBehaviour
 {
    [SerializeField] VolumeProfile _volume;
+   [SerializeField] Image _screen;
    Bloom _bloom;
 
    private void Awake() {
-    _volume.TryGet<Bloom>(out Bloom _bloom);
+    _volume.TryGet<Bloom>(out _bloom);
    }
 
    private void Start() {
       print("Hello world!");
       StartCoroutine(StartHandler());
+      StartCoroutine(WhiteScreen());
    }
 
    IEnumerator StartHandler() {
       float time = 0;
-
-      while (true) {
+      while (time < 1) {
          yield return null;
          
-         time  += Time.deltaTime;
-         // _bloom.intensity.SetValue(Mathf.Lerp(_bloom.intensity.GetValue<float>(), 500, time));
+         time  += Time.deltaTime / 20;
+         _bloom.intensity.Override(Mathf.Lerp(_bloom.intensity.GetValue<float>(), 500, time));
+      }
+   }
+
+   IEnumerator WhiteScreen() {
+      float time = 0;
+      while (time < 1) {
+         yield return null;
+         
+         time  += Time.deltaTime / 1;
+         _screen.color = new(1,1,1, time);
       }
    }
 }
