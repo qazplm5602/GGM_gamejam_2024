f7e8ecb4a05e6bc9b15d352694d83887e2103ddf
qazplm5602 <t01048187045@gmail.com>
2024-01-02 12:19:44 +0900

    총 기능

--- Assets/Minki/0B_Scripts/Player/GunController.cs
+++ Assets/Minki/0B_Scripts/Player/GunController.cs
@@ -2,18 +2,35 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
-    private Animator _animator;
+    [SerializeField] WeaponBullet _bullet;
+    IWeaponEvent eventHandler;
 
+    #if UNITY_EDITOR
+    [SerializeField] GameObject default_weapon;
+    
     private void Awake() {
-        _animator = (Animator)GetComponent("Animator");
+        SetWeapon(default_weapon);
+    }
+    #endif
+
+    public void SetWeapon(GameObject weaponEntity) {
+        var weapon = Instantiate(weaponEntity, transform);
+        eventHandler = weapon.GetComponent<IWeaponEvent>();
+        
+        eventHandler.Init(_bullet);
     }
 
     private void Update() {
-        if(Input.GetMouseButtonDown(0)) {
-            _animator.SetTrigger("Shot");
-        }
-        else if(Input.GetMouseButtonDown(1)) {
-            _animator.SetTrigger("Reload");
+        if (Input.GetMouseButtonDown(0)) {
+            eventHandler?.MouseDown();
+        } else if (Input.GetMouseButtonUp(0)) {
+            eventHandler?.MouseUp();
         }
     }
+}
+
+interface IWeaponEvent {
+    void Init(WeaponBullet _weaponBullet);
+    void MouseDown();
+    void MouseUp();
 }
