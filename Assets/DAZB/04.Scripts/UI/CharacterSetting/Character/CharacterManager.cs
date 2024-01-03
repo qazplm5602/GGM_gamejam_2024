a915739092cf4713e15a874006bcb6d13483876c
DAZB <jiwoo2426p@gmail.com>
2024-01-03 12:20:59 +0900

    가중치가 100을 초과하던거 고침

--- Assets/DAZB/04.Scripts/UI/CharacterSetting/Character/CharacterManager.cs
+++ Assets/DAZB/04.Scripts/UI/CharacterSetting/Character/CharacterManager.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    
+    public static CharacterManager instance;
+    public Animator ainm;
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetData(WeaponData weaponData) {
+
+    }
 }
