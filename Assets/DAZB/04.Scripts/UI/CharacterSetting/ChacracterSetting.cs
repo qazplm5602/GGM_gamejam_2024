4eaf719e965d54a71e798770dcaacbdddd962d76
DAZB <jiwoo2426p@gmail.com>
2024-01-03 10:59:33 +0900

    문양 가중치 올리는 매서드 만듬

--- Assets/DAZB/04.Scripts/UI/CharacterSetting/ChacracterSetting.cs
+++ Assets/DAZB/04.Scripts/UI/CharacterSetting/ChacracterSetting.cs
@@ -1,17 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class ChacracterSetting : MonoBehaviour
 {  
-    
     public void HoverMouse(GameObject obj) {
         obj.GetComponent<RectTransform>().DOScale(new Vector3(0.85f, 0.85f, 0.85f), 0.15f);
     }
 
     public void UnHoverMouse(GameObject obj) {
         obj.GetComponent<RectTransform>().DOScale(new Vector3(0.7f + 0.3f, 0.7f + 0.3f, 0.7f + 0.3f), 0.15f);
+    }
+
+    public void NextScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
     }
 }
