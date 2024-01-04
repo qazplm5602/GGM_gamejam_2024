443a3f2882bbdc26df349543fbc128d362e7e3f7
DAZB <jiwoo2426p@gmail.com>
2024-01-04 14:26:11 +0900

    3번쨰 페이지 완성

--- Assets/DAZB/04.Scripts/UI/Summary/TwoPage.cs
+++ Assets/DAZB/04.Scripts/UI/Summary/TwoPage.cs
@@ -13,13 +13,13 @@ public class TwoPage : MonoBehaviour
         texts[2].text = "Two pair: " + CheckCard.instance.rankingCounter[2];
         texts[3].text = "Triple: " + CheckCard.instance.rankingCounter[3];
         texts[4].text = "Straight: " + CheckCard.instance.rankingCounter[4];
-        texts[5].text = "BackStraight: " + CheckCard.instance.rankingCounter[5];
+        texts[5].text = "Back straight: " + CheckCard.instance.rankingCounter[5];
         texts[6].text = "Mountain: " + CheckCard.instance.rankingCounter[6];
         texts[7].text = "Flush: " + CheckCard.instance.rankingCounter[7];
         texts[8].text = "Full house: " + CheckCard.instance.rankingCounter[8];
         texts[9].text = "Four card: " + CheckCard.instance.rankingCounter[9];
         texts[10].text = "Straight flush: " + CheckCard.instance.rankingCounter[10];
-        texts[11].text = "BackStraight flush: " + CheckCard.instance.rankingCounter[11];
+        texts[11].text = "Back straight flush: " + CheckCard.instance.rankingCounter[11];
         texts[12].text = "Royal straight flush: " + CheckCard.instance.rankingCounter[12];
     }
 }
