78fe022a734ca706c699c6400ab8eb294fcd9be6
qazplm5602 <t01048187045@gmail.com>
2024-01-04 22:45:41 +0900

    모양 ㅁㄴㅇㄹ

--- Assets/DomiTest/Scripts/WeaponSkillHandlers.cs
+++ Assets/DomiTest/Scripts/WeaponSkillHandlers.cs
@@ -492,7 +492,8 @@ public class WeaponSkillHandlers : MonoBehaviour
 
         // 카드 안삼 만듬
         var bullets = new List<GameObject>();
-        string[] cardLoop = {"spade_A", "heart_A", "diamond_A", "clover_A"};
+        string[] cardLoop = {"spade_A", "diamond_A", "heart_A", "clover_A"};
+        CardShape[] cardShapeLoop = {CardShape.SPADE, CardShape.DIAMOND, CardShape.HEART, CardShape.CLUB };
         int domi_a = cardLoop.Length - 1;
         for (int i = 0; i < 3; i++)
         {
@@ -513,7 +514,7 @@ public class WeaponSkillHandlers : MonoBehaviour
 
                 // 콜백 생성
                 var bullet = item.GetComponent<CardWeaponBullet>();
-                bullet.OnCallback += CreateFlushCardHandler(saveShape, item.transform.position, saveDamage, item);
+                bullet.OnCallback += CreateFlushCardHandler(cardShapeLoop[domi_a], item.transform.position, saveDamage, item);
                 bullet.damage = saveDamage_default;
 
                 bullets.Add(item);
