8983ca02ef21616788f473a27b5d6997578222b4
mki821 <alexalex0821@naver.com>
2024-01-05 01:51:37 +0900

    🎞️크레딧 수정

--- Assets/Minki/0B_Scripts/ETC/Credit.cs
+++ Assets/Minki/0B_Scripts/ETC/Credit.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class Credit : MonoBehaviour
 {
+    [SerializeField] private GameObject _button;
     [SerializeField] private float _speed = 0.5f;
 
+    private bool _first = true;
+
     private void Update() {
-        if(transform.position.y >= 57f) return;
+        if(transform.position.y >= 57f) {
+            if(_first) {
+                _first = false;
+                ShowButton();
+            }
+            return;
+        }
 
         transform.position += Vector3.up * Time.deltaTime * _speed;
 
@@ -13,4 +22,10 @@ public class Credit : MonoBehaviour
         else if(Input.GetMouseButton(1)) _speed = 0f;
         else _speed = 1; 
     }
+
+    private void ShowButton() {
+        _button.SetActive(true);
+    }
+
+    public void GotoMain() => UnityEngine.SceneManagement.SceneManager.LoadScene(0);
 }
