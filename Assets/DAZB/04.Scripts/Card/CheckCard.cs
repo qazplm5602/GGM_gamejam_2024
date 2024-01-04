9bf18c44d6428db666dbae47bb2a7960fdb981ec
DAZB <jiwoo2426p@gmail.com>
2024-01-04 19:35:16 +0900

    레벨업 두번 버그 고침

--- Assets/DAZB/04.Scripts/Card/CheckCard.cs
+++ Assets/DAZB/04.Scripts/Card/CheckCard.cs
@@ -31,7 +31,7 @@ public class CheckCard : MonoBehaviour
     }
 
     public void DrawCard() {
-        GetStraight();
+        //SetCard();
         rankingInfo = CheckedCard();
         rankingCounter[(int)rankingInfo.ranking]++;
         ShuffleCards();
@@ -64,12 +64,12 @@ public class CheckCard : MonoBehaviour
         drawCount++;
     }
 
-    private void GetStraight() {
+    private void SetCard() {
         playerCards[0] = new Card(CardShape.SPADE, 6);
         playerCards[1] = new Card(CardShape.SPADE, 2);
         playerCards[2] = new Card(CardShape.SPADE, 3);
         playerCards[3] = new Card(CardShape.SPADE, 4);
-        playerCards[4] = new Card(CardShape.SPADE, 5);
+        playerCards[4] = new Card(CardShape.HEART, 5);
     }
 
     private void ShuffleCards() {
@@ -81,54 +81,54 @@ public class CheckCard : MonoBehaviour
 
     private RankingInfo CheckedCard() {
         if (HandRankings.instance.RoyalStraightFlushCheck(playerCards).Item1) {
-            //print(HandRankings.instance.RoyalStraightFlushCheck(playerCards).Item2.cardShape + " Royal Straight Flush");        
+            print(HandRankings.instance.RoyalStraightFlushCheck(playerCards).Item2.cardShape + " Royal Straight Flush");        
             return new RankingInfo(Ranking.ROYALSTRAIGHTFLUSH, HandRankings.instance.RoyalStraightFlushCheck(playerCards).Item2);
         }
         if (HandRankings.instance.BackStraightFlushCheck(playerCards).Item1) {
-            //print(HandRankings.instance.BackStraightFlushCheck(playerCards).Item2.cardShape + " Back Straight Flush");
+            print(HandRankings.instance.BackStraightFlushCheck(playerCards).Item2.cardShape + " Back Straight Flush");
             return new RankingInfo(Ranking.BACKSTRAIGHTFLUSH, HandRankings.instance.BackStraightFlushCheck(playerCards).Item2);
         }
         if (HandRankings.instance.StraightFlushCheck(playerCards).Item1) {
-            //print(HandRankings.instance.StraightFlushCheck(playerCards).Item2.cardShape + " Straight Flush");
+            print(HandRankings.instance.StraightFlushCheck(playerCards).Item2.cardShape + " Straight Flush");
             return new RankingInfo(Ranking.STRAIGHTFLUSH, HandRankings.instance.StraightFlushCheck(playerCards).Item2);
         }
         if (HandRankings.instance.FourCardCheck(playerCards).Item1) {
-            //print(HandRankings.instance.FourCardCheck(playerCards).Item2.cardNumber + " Four card");
+            print(HandRankings.instance.FourCardCheck(playerCards).Item2.cardNumber + " Four card");
             return new RankingInfo(Ranking.FOURCARD, HandRankings.instance.FourCardCheck(playerCards).Item2);
         }
         if (HandRankings.instance.FullHouseCheck(playerCards)) {
-            //print(" FullHouse");
+            print(" FullHouse");
             return new RankingInfo(Ranking.FULLHOUSE);
         }
         if (HandRankings.instance.FlushCheck(playerCards).Item1) {
-            ///print(HandRankings.instance.FlushCheck(playerCards).Item2.cardShape + " Flush");
+            print(HandRankings.instance.FlushCheck(playerCards).Item2.cardShape + " Flush");
             return new RankingInfo(Ranking.FLUSH, HandRankings.instance.FlushCheck(playerCards).Item2);
         }
         if (HandRankings.instance.MountainCheck(playerCards)) {
-            //print(" Mountine");
+            print(" Mountine");
             return new RankingInfo(Ranking.MOUNTAIN);
         }
         if (HandRankings.instance.BackStraightCheck(playerCards)) {
-            ///print(" Back straight");
+            print(" Back straight");
             return new RankingInfo(Ranking.BACKSTRAIGHT);
         }
         if (HandRankings.instance.StraightCheck(playerCards)) {
-            //print(" Straight");
+            print(" Straight");
             return new RankingInfo(Ranking.STRAIGHT);
         }
         if (HandRankings.instance.TripleCheck(playerCards).Item1) {
-            //print(HandRankings.instance.TripleCheck(playerCards).Item2.cardNumber + " Triple");
+            print(HandRankings.instance.TripleCheck(playerCards).Item2.cardNumber + " Triple");
             return new RankingInfo(Ranking.TRIPLE, HandRankings.instance.TripleCheck(playerCards).Item2);
         }
         if (HandRankings.instance.TwoPairCheck(playerCards).Item1) {
-            //print(/* HandRankings.instance.TwoPairCheck(playerCards).Item2[0].cardNumber +  */" Two pair");
+            print(/* HandRankings.instance.TwoPairCheck(playerCards).Item2[0].cardNumber +  */" Two pair");
             return new RankingInfo(Ranking.TWOPAIR, HandRankings.instance.TwoPairCheck(playerCards).Item2);
         }
         if (HandRankings.instance.OnePairCheck(playerCards).Item1) {
-            //print(HandRankings.instance.OnePairCheck(playerCards).Item2.cardNumber + " One pair");
+            print(HandRankings.instance.OnePairCheck(playerCards).Item2.cardNumber + " One pair");
             return new RankingInfo(Ranking.ONEPAIR, HandRankings.instance.OnePairCheck(playerCards).Item2);
         }
-        //print(HandRankings.instance.HighCardCheck(playerCards).Item2.cardNumber + " High Card");
+        print(HandRankings.instance.HighCardCheck(playerCards).Item2.cardNumber + " High Card");
         return new RankingInfo(Ranking.HIGHCARD, HandRankings.instance.HighCardCheck(playerCards).Item2);
     }
 }
