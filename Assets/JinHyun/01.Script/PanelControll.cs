d3c9c80495ef8968947836d0f27416b2dd0c4f25
6equal0 <6equal0@gmail.com>
2024-01-10 22:04:08 +0900

    없어도되게함
    gizmozzizzi

--- Assets/JinHyun/01.Script/PanelControll.cs
+++ Assets/JinHyun/01.Script/PanelControll.cs
@@ -13,14 +13,16 @@ public class PanelControll : MonoBehaviour
             item.SetActive(false);
         }
         clickShield.SetActive(false);
-        playerBulletSys.fireDisable = false;
+        if (playerBulletSys)
+            playerBulletSys.fireDisable = false;
         Time.timeScale = 1f;
     }
 
     private void OnEnable()
     {
         clickShield.SetActive(true);
-        playerBulletSys.fireDisable = true;
+        if (playerBulletSys)
+            playerBulletSys.fireDisable = true;
         Time.timeScale = 0;
     }
 }
